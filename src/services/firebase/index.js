9942b99d53fd50a417d1562a42a206dfd90a5cac
Gonzalo Parada <gonzaparada96@gmail.com>
2022-09-14 17:33:58 -0300

    actualizacion firebase/index

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,7 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "./firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
